d2c44f7fecc75c96694bf50d63aab53fa3e7df19
ismael-soler <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@github.com>
2022-06-06 09:21:27 -0700

    commit

---
+++ hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * main - displays test with the stderr
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n", file=sys.stderr);
+
+	return (1);
+}
