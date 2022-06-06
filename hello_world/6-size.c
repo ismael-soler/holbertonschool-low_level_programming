52c3da9afc1915152f08934285539489133affd4
ismael-soler <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@github.com>
2022-06-06 07:56:35 -0700

    commit

---
+++ hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ *Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	printf("Size of a char: %d",sizeof(char));
+	printf("\nSize of an int: %d",sizeof(int));
+	printf("\nSize of a long int: %d",sizeof(long int));
+	printf("\nSize of a long long int: %d",sizeof(long long int));
+	printf("\nSize of a float: %d\n",sizeof(float));
+
+	return (0);
+
+}
