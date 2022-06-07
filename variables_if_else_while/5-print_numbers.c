f06b4d6ba60112d14d1c62859c8af41ee803c9f8
ismael-soler <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@github.com>
2022-06-07 09:43:47 -0700

    commit

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Prints all numbers single digit
- *
- * Return 0 on success
- */
+ *  * main - prints all single digit numbers of base
+ *   * 10 starting from 0, followed by a new line
+ *    * Return: Always 0 (Success)
+ *     */
 
 int main(void)
 {
