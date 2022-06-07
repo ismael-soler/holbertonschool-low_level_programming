592b9a3d2aaea41eabce309dcf01c59c4b19f19c
ismael-soler <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@github.com>
2022-06-07 09:50:19 -0700

    commit

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Print all digits again
+ *
+ * Return: 0 on Success
+ *
+ */
+
+int main(void)
+{
+	int n
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
