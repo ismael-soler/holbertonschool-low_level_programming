4e5444cc758e4fb4ba26a63de64a85b23d9ffbbc
ismael-soler <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@github.com>
2022-06-07 07:06:50 -0700

    commit

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Displays the whole alphabet in lowercase
+ *
+ * Return: 0 in Succes
+ *
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		(ch++);
+	}
+	putchar('\n');
+	return (0);
+}
