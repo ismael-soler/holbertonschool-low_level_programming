059de5dd219cb3329b91194579e8d88d779dcc67
ismael-soler <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@github.com>
2022-06-07 17:39:15 -0700

    commit

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,12 +10,12 @@
 
 int main(void)
 {
-	int n = 0;
+	int n = '0';
 
-	while (n < 10)
+	while (n <= '9')
 	{
-		putchar(n + '0');
-		if (n < 9)
+		putchar(n);
+		if (n < '9')
 		{
 			putchar(44);
 			putchar(32);
