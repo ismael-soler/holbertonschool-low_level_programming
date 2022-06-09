815be2c3627057878acf451eca29a0b274a7a2af
ismael-soler <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@github.com>
2022-06-09 07:22:09 -0700

    commit

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* print_sign - prints if positive or negative
+* @n: number
+*
+* Return: 1 if positive, 0 if zero, and -1 if negative
+*
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return ('-1');
+	}
+}
