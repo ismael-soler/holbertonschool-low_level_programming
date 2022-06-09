9fb91bd9603335b0e34a812d0239535beeba5dde
ismael-soler <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@github.com>
2022-06-09 06:18:28 -0700

    commit

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	char contador;
+	for (contador = '0'; contador <= '9'; contador++)
+	{
+		print_alphabet();
+	}
+}
