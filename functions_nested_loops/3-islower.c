79fb1b3e56f3a6a02b3592240871692139aea715
ismael-soler <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@github.com>
2022-06-09 06:40:22 -0700

    commit

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *  _islower - does some stuff idk im tired
+ *
+ * Return: 1 if lowercase, 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
