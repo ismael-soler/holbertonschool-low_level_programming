bbe362c7edcfc09fd53b8cd9a561af8cbcd93e57
ismael-soler <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@github.com>
2022-06-09 06:53:30 -0700

    commit

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - detects for alphabetic character or not 
+ * @c: character
+ * Return: 1 if alpha 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
