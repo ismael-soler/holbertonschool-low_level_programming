fc85a12dffc75cdb2ca0e6e3094c6159b04d1774
ismael-soler <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@github.com>
2022-06-10 07:33:57 -0700

    commit

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -14,7 +14,7 @@ void times_table(void)
 	{
 		for (y = '0'; y <= '9'; y++)
 		{
-			n = (x * y);
+			n = x * y;
 			if (y == '0')
 			{
 				_putchar(n + '0');
@@ -22,9 +22,9 @@ void times_table(void)
 			else if (n <= '9') 
 			{
 				_putchar(' ');
-				_putchar((n % 10) + '0');
+				_putchar(n + '0');
 			}
-			else if (y <= '9')
+			else if (n > '9')
 			{
 				_putchar((n / 10) + '0');
 				_putchar((n % 10) + '0');
@@ -35,11 +35,7 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 			}
-			else
-			{
-				_putchar('\n');
-			}
 		}
-
+		_putchar('\n');
 	}
 }
