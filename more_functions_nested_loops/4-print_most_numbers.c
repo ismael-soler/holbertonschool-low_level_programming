46e0a204540d03be49ec2c77ce851ee1000a99b5
ismael-soler <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@github.com>
2022-06-11 13:09:09 -0700

    commit

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i == '2' || i == '4')
+		_putchar(i);
+	}
+_putchar('\n');
+}
