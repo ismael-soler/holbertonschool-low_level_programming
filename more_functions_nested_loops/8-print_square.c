2c0c04b2e437affffaaf1ca6b16a9af9d60788a9
ismael-soler <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@github.com>
2022-06-11 15:32:54 -0700

    commit

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ * @size: size of the square
+ * Return: 0
+ */
+
+void print_square(int size)
+{
+	int size;
+	int h, v;
+
+	h = v = size;
+	if (size > 0)
+	{
+		for (v--)
+		{
+			while (h--)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+			h = size;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
