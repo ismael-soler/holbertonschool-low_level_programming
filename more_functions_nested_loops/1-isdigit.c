61df44570c10461d309d4dfdc320a27e5e73e3a6
ismael-soler <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@github.com>
2022-06-11 12:44:09 -0700

    commit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isdigit - returns 1 if c is a digit
+ * @c: digit
+ * Return: 0 
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' || c <= '0')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
