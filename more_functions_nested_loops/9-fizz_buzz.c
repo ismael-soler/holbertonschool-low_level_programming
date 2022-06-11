cbc1b362c5658fe718b134b8ff33a95f59e831bb
ismael-soler <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@github.com>
2022-06-11 15:51:27 -0700

    commit

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - prints from 1 to 100 but
+ * for multiples of three print Fizz and for multiples
+ * of five print Buzz, if mulitple of both print FizzBuzz.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n; /*Number to print*/
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+	}
+	return (0);
+}
+
