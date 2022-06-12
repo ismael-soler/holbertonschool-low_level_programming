71231602dfb2382d8310e3a29cdebe3ce138f4c6
ismael-soler <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@github.com>
2022-06-12 09:27:08 -0700

    commit

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,50 +1,50 @@
 #include "main.h"
 
 /**
- * print_times_table - prints n times table
- * @n: number of tables to print
+ * print_times_table - pritns n times table
+ * @n: times to print
  */
 
 void print_times_table(int n)
 {
-	int x, y;
-	int number;
-
-	if (n <= 15 || n >= 0)
+	int column, line;
+	int print;
+	
+	if (n <= 15 && n >= 0)
 	{
-		x = 0;
-		while (x < n)
+	column = 0;
+	while (column <= n)
+	{
+		line = 0;
+		while (line <= n)
 		{
-			y = 0;
-			while (y < n)
+			print = column * line;
+			if (n > 9)
 			{
-				number = x * y;
-				if (number > 9)
-				{
-					_putchar(number / 10 + '0');
-					_putchar(n % 10 + '0');
-				}
-
-				else if (y != 0)
-				{
-					_putchar(' ');
-					_putchar(n + '0');
-				}
-
-				else
-				{
-					_putchar(n + '0');
-				}
-
-				if (y != 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-				y++;
+				_putchar(print / 10 + '0');
+				_putchar(print % 10 + '0');
 			}
-			_putchar('\n');
-			x++;
+
+			else if (line != 0)
+			{
+				_putchar(' ');
+				_putchar(n + '0');
+			}
+
+			else
+			{
+				_putchar(n + '0');
+			}
+
+			if (line != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			line++;
 		}
+		_putchar('\n');
+		column++;
+	}
 	}
 }
