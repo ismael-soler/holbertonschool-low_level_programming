1b22958ee17c9cf786a694e7ef3d9eea524ed39c
ismael-soler <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@github.com>
2022-06-12 12:39:56 -0700

    commit

---
+++ functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints first 50 finbonacci numbers starting with 1 and 2
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long pre2; /* last printed number */
+	unsigned long print, old_print; /* number to print / printed number placeholder */
+	int i; /* iteratiorns */
+
+	pre2 = 1;
+	print = 1;
+	for (i = 0; i < 98; i++)
+	{
+		printf("%ld, ", print);
+		old_print = print;
+		print = print + pre2;
+		pre2 = old_print;
+	}
+return (0);
+}
