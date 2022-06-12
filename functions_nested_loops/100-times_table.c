01bae215157a4c85889fe783adf18daa7ed399c9
ismael-soler <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@github.com>
2022-06-12 09:43:38 -0700

    commit

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -19,14 +19,23 @@ void print_times_table(int n)
 		while (line <= n)
 		{
 			print = column * line;
-			if (n > 9)
+			if (print > 99)
 			{
+				_putchar(print / 100 + '0');
+				_putchar((print - 100) / 10 + '0');
+				_putchar(print % 10 + '0');
+			}
+
+			else if (print > 9)
+			{
+				_putchar(' ');
 				_putchar(print / 10 + '0');
 				_putchar(print % 10 + '0');
 			}
 
-			else if (line <= 0)
+			else if (line != 0)
 			{
+				_putchar(' ');
 				_putchar(' ');
 				_putchar(print + '0');
 			}
