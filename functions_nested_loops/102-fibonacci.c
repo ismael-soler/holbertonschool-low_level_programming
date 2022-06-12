8dde3803ae311359f08a3611943085467e3c89bc
ismael-soler <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@github.com>
2022-06-12 11:43:07 -0700

    commit

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -7,26 +7,25 @@
 
 int main(void)
 {
-	int pre1, pre2; /* last two printed nubers */
+	int pre2; /* last printed number */
 	int print, old_print; /* number to print / printed number placeholder */
 	int check; /* for checking if the first number starts with 1 or 2 */
+	int i; /* number of fibonacci numbers to scan */
 
-	pre1 = 0;
 	pre2 = 1;
 	print = 0;
-	for (int i = 0; i < 50; i++)
+	for (i = 0; i < 50; i++)
 	{
 		check = print;
 		while (check > 10)
 		{
-			check / 10;
+			check = check / 10;
 		}
 		if (check == 1 || check == 2)
 		{
 			printf("%d, ", print);
 			old_print = print;
 			print = print + pre2;
-			pre1 = pre2;
 			pre2 = old_print;
 		}
 		else
