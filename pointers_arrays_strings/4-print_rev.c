60259ec3869d13e79a4ed0831dbdc307fee9a9ab
ismael-soler <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@github.com>
2022-06-14 10:11:29 -0700

    commit

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * print_rev - prints a string in reverse followed by a new line
+ * @s: string to print
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+	int i;
+
+	while (*s--)
+	{
+		len++;
+	}
+	for (i = 0; i >= 0; i++)
+	{
+		printf("%s",s[len - i]);
+	}
+}
