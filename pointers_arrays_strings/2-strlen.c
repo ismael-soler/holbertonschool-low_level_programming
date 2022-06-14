296c3913e4dbb189353a14bd530cf6211c8ecda4
ismael-soler <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@github.com>
2022-06-14 09:41:25 -0700

    commit

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * _strlen - returns the lenght of the string 
+ * @s: string
+ */
+
+int _strlen(char *s)
+{
+	int len;
+	
+	len = 0;
+	while (*s !='\0')
+	{
+		s++;
+		len++;
+	}
+	return (len);
+}
