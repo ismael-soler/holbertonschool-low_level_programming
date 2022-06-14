67aa43452a41dfadf82e42f47c5d95123f01b25e
ismael-soler <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@github.com>
2022-06-14 09:23:15 -0700

    commit

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* swap_int - swaps the value of the twoo integrers
+* @a: first integrer
+* @b: second integrer
+*/
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+	temp = &a;
+	&a = &b;
+	&b = temp;
+}
