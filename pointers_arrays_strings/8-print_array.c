31296941b7703e55e8fc0f74596c143dba7b833d
ismael-soler <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@github.com>
2022-06-15 11:22:33 -0700

    commit

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -10,13 +10,13 @@
 void print_array(int *a, int n)
 {
 	int i;
-	
+
 	n--;
 	for (i = 0; i <= n; i++)
 	{
 		if (i < n)
 			printf("%d, ", a[i]);
-		else if (i = n)
+		else if (i == n)
 			printf("%d\n", a[i]);
 	}
 }
