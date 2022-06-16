3d3c42eb877c7486ff1954b10ec1573f228e5ec1
ismael-soler <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@github.com>
2022-06-16 10:26:56 -0700

    commit

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncat - appends the string in src to the one in dest
+ * @dest: destination
+ * @src: source
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i]; i++)
+	{}
+	for (j = 0; n >= 0; j++, i++, n--)
+		dest[i] = src[j];
+	src[j] = '\0';
+	return (dest);
+}
+
