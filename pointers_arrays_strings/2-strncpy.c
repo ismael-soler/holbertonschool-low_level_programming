ffc5315acd151e0906cfc1ab35d2425ff9ae035e
ismael-soler <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@github.com>
2022-06-16 12:13:46 -0700

    commit

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -16,10 +16,14 @@ char *_strncpy(char *dest, char *src, int n)
 		return (dest);
 	for (i = 0; src[i]; i++)
 		;
-	for (i = 0; src[i]; i++)
-		dest[i] = src[i];
 	if (i < n)
+		for (i = 0; src[i]; i++)
+			dest[i] = src[i];
 		for (; i <= n; i++)
 			dest[i] = '\0';
+	else if (i > n)
+		for (i = 0; i < n; i++)
+			dest[i] = src[i];
+		dest[i] = '\0';
 	return (dest);
 }
