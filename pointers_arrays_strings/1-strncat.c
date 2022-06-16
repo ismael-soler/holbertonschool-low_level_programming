7436007812c4a1b860ad91536bd0a902b5bd5fc4
ismael-soler <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@github.com>
2022-06-16 10:42:58 -0700

    commit

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -11,12 +11,14 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
-
-	for (i = 0; dest[i]; i++)
-	{}
-	for (j = 0; j < n; j++, i++)
-		dest[i] = src[j];
-	dest[i] = '\0';
-	return (dest);
+	if (n > 0)
+	{
+		for (i = 0; dest[i]; i++)
+		{}
+		for (j = 0; j < n; j++, i++)
+			dest[i] = src[j];
+		dest[i] = '\0';
+	}
+		return (dest);
 }
 
