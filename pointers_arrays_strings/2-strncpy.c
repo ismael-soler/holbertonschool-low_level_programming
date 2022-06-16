bfdd523fb3d5925130d1d7d958992d9c30b71809
ismael-soler <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@github.com>
2022-06-16 11:21:10 -0700

    commit

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -13,11 +13,11 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	for (i = 0; src[i]; i++)
-		src[i];
-	if (i < n)
+		; /* calculo distancia */
+	if (i < n) /* si el contenido es mas chico que n */
 		n = i;
 	if (n > 0)
-		for (i = 0; i < n; i++)
+		for (i = 0; i < n; i++) /* copiacion */
 			dest[i] = src[i];
 	return (dest);
 }
