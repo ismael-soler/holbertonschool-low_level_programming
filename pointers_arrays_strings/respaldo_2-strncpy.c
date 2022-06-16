f328e2e1054138ecc932235547710cd2301d150d
ismael-soler <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@github.com>
2022-06-16 16:42:56 -0700

    commit

--- pointers_arrays_strings/respaldo_2-strncpy.c
+++ pointers_arrays_strings/respaldo_2-strncpy.c
@@ -12,18 +12,24 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
+	for (i = 0; *(src + i); i++)
+		;
 	if (n == 0)
 		;
-	else if (src[0] != '\0')
+	else if (src[0] == '0')
+		for (i = 0; i < n; i++)
+			src[i] = ;
+	else if (i < n)
+	{
+		for (i = 0; src[i]; i++)
+			dest[i] = src[i];
+		for (; i <= n; i++)
+			dest[i] = '\0';
+	}
+	else if (i > n)
 	{
 		for (i = 0; i < n; i++)
 			dest[i] = src[i];
-		if (i < n)
-			for (; i < n; i++)
-				dest[i] = '\0';
 	}
-	else if (src[0] == '\0')
-		for (i = 0; i < n; i++)
-			dest[i] = '\0';
 	return (dest);
 }
