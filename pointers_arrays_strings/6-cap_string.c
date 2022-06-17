e5a522dbec934d6d2c9a9bbf61b1598c4924c161
ismael-soler <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@github.com>
2022-06-17 08:37:05 -0700

    commit

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: string
+ * Return: str
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+		if (str[i] == ',', ';', '.', '!', '?', '"', '(', ')', '{',
+				'}', 32, 9, '\n' && str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			str[i + 1] -= 32;
+	return (str);
+}
