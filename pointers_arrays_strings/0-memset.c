17d10f7540a849baa9d30a409c5f855ae9320974
ismael-soler <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@github.com>
2022-06-17 10:58:01 -0700

    commit

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,21 +1,22 @@
 #include "main.h"
 
 /**
- * _memset - fills the first n bytes of the moemory area with a constant byte
+ * _memset - fills the first n bytes of the memory area with a constant byte
  * @s: string
  * @b: constant byte
  * @n: number of bytes to fill
+ * Return: s
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
 	if (n <= 0)
 		;
 	else
 		for (i = 0; i < n; i++)
-			s[i] = 'b'
+			s[i] = b;
 	return (s);
 }
 
