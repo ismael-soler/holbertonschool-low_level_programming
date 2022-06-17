b78cdceb08e184b47e37d2549c788c7ca31b8e96
ismael-soler <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@github.com>
2022-06-17 06:51:30 -0700

    commit

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - inverts the content of an array
+ * @a: array
+ * @n: numbers of elements of the array to swap
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, len;
+	int aux[999];
+
+	if (n <= 0)
+		;
+	else
+		for (i = 0, len = 0; i < n; i++, len++)
+			a[i] = aux[i];
+		for (; i >= 0, i--)
+			a[len - i] = aux[i]
+}
