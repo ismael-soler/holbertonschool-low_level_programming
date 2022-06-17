2c97727a14dc2173f6d3a1c0bd2c131781847a1b
ismael-soler <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@github.com>
2022-06-17 08:38:09 -0700

    commit

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -11,8 +11,8 @@ char *cap_string(char *str)
 	int i;
 
 	for (i = 0; str[i]; i++)
-		if (str[i] == ',', ';', '.', '!', '?', '"', '(', ')', '{',
-				'}', 32, 9, '\n' && str[i + 1] >= 'a' && str[i + 1] <= 'z')
+		if (str[i] == ',' ';' '.' '!' '?' '"' '(' ')' '{'
+				'}' 32 9 '\n' && str[i + 1] >= 'a' && str[i + 1] <= 'z')
 			str[i + 1] -= 32;
 	return (str);
 }
