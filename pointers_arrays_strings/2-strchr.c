648920b164d1b914124ceac5a698a737fd63272e
ismael-soler <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@github.com>
2022-06-17 12:13:39 -0700

    commit

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -12,8 +12,9 @@ char *_strchr(char *s, char c)
 	int i;
 
 	for (i = 0; s[i]; i++)
-	{	if (s[i] == '\0')
-			return ((s + i));
+	{
+		if (s[i] == '\0')
+			return (s);
 		else if (s[i] == c)
 			return ((s + i));
 	}
