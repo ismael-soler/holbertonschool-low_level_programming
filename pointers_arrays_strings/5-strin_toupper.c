939680848cbd72f7e9fa2c3d1a4fa3355ceee68c
ismael-soler <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@github.com>
2022-06-17 08:19:03 -0700

    commit

---
+++ pointers_arrays_strings/5-strin_toupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * Return: str
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+	
+	for (i = 0; str[i]; i++)
+		if (str[i] > a && str[i] < z)
+			str[i] -= 32;
+	return (str);
+}
