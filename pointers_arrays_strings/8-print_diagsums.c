64e37c4deae72f257606c81b2dfb68b4e3097031
ismael-soler <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@github.com>
2022-06-19 07:59:00 -0700

    commit

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_diagsums - prints the sum of the two diagonals of a square matrix of
@@ -7,14 +8,20 @@
  * @size: size of the square
  */
 
-void print_diagsum(int *a, int size)
+void print_diagsums(int *a, int size)
 {
-	int i, j;
-	unsigned int sum_a, sum_b;
+	int i;
+	int sum1, sum2;
+	int len;
 
-	for (i = 0, j = 0; i < size; i++, j++)
-		sum_a += a[i][j];
-	for (; i> 0; i--, j--)
-		sum_b += a[i][j];
-	printf("%d, %d", sum_a, sum_b);
+	sum1 = sum2 = 0;
+	len = (size * size);
+	for (i = 0; i < (size * size); i++)
+	{
+		if (i % (size - 1) == 0 && i < len - 1 && i > 0)
+			sum2 += *(a + i);
+		if (i % (size + 1) == 0 || i == 0)
+			sum1 += *(a + i);
+	}
+	printf("%d, %d\n", sum1, sum2);
 }
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
 void reset_to_98(int *n);
+void print_diagsums(int *a, int size);
 void print_chessboard(char (*a)[8]);
 char *_strstr(char *haystack, char *needle);
 char *_strpbrk(char *s, char *accept);
