a745e750c01c5be296b13af02038eab083aa6238
ismael-soler <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@github.com>
2022-06-21 11:32:34 -0700

    commit

---
+++ argc_argv/4-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the sum of all arguments
+ * @argc: argument count
+ * @argv: argument value
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i = 0;
+
+	if (
+}
