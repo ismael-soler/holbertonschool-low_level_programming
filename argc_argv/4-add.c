99d8fe06632bcd96519f4eec322c115870333c0e
ismael-soler <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@github.com>
2022-06-21 12:46:33 -0700

    commit

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -9,8 +9,21 @@
 
 int main(int argc, char *argv[])
 {
-	int sum = 0;
-	int i = 0;
+	int i, j;
+	int num;
 
-	if (
+	for (i = 1; i < argc; i++)
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (argv[i][j] < 48 || argv[i][j] > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+	for (i = 1; i < argc; i++)
+		num += atoi(argv[i]);
+	printf("%d\n", num);
+return (0);
 }
