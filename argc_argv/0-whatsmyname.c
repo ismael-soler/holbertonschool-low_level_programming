62d72d5f7404182e9059e96385e0dfede360bb84
ismael-soler <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@github.com>
2022-06-21 10:11:40 -0700

    commit

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * mynameis - prints its own name
+ * @argc: argument count
+ * @argv: arguments passed to main as an array of strings
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
