d25f1371d9e3ce47110f16d8816c48591296b7f7
ismael-soler <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@github.com>
2022-06-23 06:30:59 -0700

    commit

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+
+/**
+ * create_array - creats an array of chars and init with a specific char
+ * @size: size of the array
+ * @c: character
+ * Return: NULL if size = 0
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ar;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+	ar = malloc(size * sizeof(char));
+	for (i = 0; ar[i]; i++)
+		ar[i] = c;
+	return (ar);
+}
+
