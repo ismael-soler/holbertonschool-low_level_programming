104e4fb409668603845b3613baa44435c0a7537f
ismael-soler <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@github.com>
2022-06-23 07:37:07 -0700

    commit

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -17,6 +17,8 @@ char *_strdup(char *str)
 	for (size = 0; str[size]; size++)
 		;
 	ar = malloc((size + 1) * sizeof(char));
+	if (ar == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		ar[i] = str[i];
 	return (ar);
