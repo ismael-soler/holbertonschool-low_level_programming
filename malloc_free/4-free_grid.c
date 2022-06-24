66135503ca120a71a112defd657fa031628b5a1f
ismael-soler <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@github.com>
2022-06-24 08:03:24 -0700

    commit

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a 2 dimensional grid previously created
+ * @grid: grid
+ * @height: height
+ */
+
+void free_grid(int **grid, int height);
+{
+	int i;
+
+}
