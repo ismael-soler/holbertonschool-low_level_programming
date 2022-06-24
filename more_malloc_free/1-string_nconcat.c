3be12efe9c219bf1b388108713febce7f5779080
ismael-soler <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@github.com>
2022-06-24 09:44:33 -0700

    commit

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -30,7 +30,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (i = 0; s1[i]; i++)
 		p[i] = s1[i];
-	for (j = 0; j <= n; j++, i++)
+	for (j = 0; j < n; j++, i++)
 		p[i] = s2[j];
 	p[i] = '\0';
 
