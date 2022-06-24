9b1d4e7be0d4fa6deee13969e09a6aa0a1c177b0
ismael-soler <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@github.com>
2022-06-24 11:40:37 -0700

    commit

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * array_range - creates an array of integrers
+ * @min: min
+ * @max: max
+ * Return: pointer to the newly created array
+ */
+
+int *array_range(int min, int max)
+{
+	int *p;
+	int size, i;
+
+	if (min > max)
+		return (NULL);
+
+	size = max - min;
+	p = malloc(size + 1 * sizeof(int));
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i <= size; i++, min++)
+		p[i] = min;
+	return (p);
+}
