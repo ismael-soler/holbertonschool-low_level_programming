f1079ca1ab37c7856eebae0c1241de3b01dfcdb8
ismael-soler <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@github.com>
2022-06-24 10:58:15 -0700

    commit

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	a = malloc(nmemb * size);
 	if (a == NULL)
 		return (NULL);
-	for (i = 0; a[i]; i++)
+	for (i = 0; i <= nmemb; i++)
 		a[i] = 0;
 	return (a);
 }
