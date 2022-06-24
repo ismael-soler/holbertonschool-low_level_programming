cad8718dc7f9618eb8c6ba373cd09cac415e1b01
ismael-soler <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@github.com>
2022-06-24 07:41:16 -0700

    commit

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -14,13 +14,21 @@ int **alloc_grid(int width, int height)
 
 	ar = malloc(height * sizeof(*ar));
 	if (ar == NULL)
+	{
+		free (ar);
 		return (NULL);
+	}
 
 	for (i = 0; i < height; i++)
 	{
 		ar[i] = malloc(width * sizeof(**ar));
 		if (ar[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free (ar[i]);	
+			free (ar);
 			return (NULL);
+		}
 		for (j = 0; j < width; j++)
 			ar[i][j] = 0;
 	}
