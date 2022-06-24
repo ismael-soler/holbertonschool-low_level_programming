16c9f4d09656307c94265aa4a2ef9b4eff7b4773
ismael-soler <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@github.com>
2022-06-24 11:21:57 -0700

    commit

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -4,6 +4,7 @@ int _putchar(char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 void free_grid(int **grid, int height);
+void *malloc_checked(unsigned int b);
 char *create_array(unsigned int size, char c);
 int **alloc_grid(int width, int height);
 #endif
