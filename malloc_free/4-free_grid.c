73dc9775ca2a22d08d107116b76e17514cb83710
ismael-soler <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@github.com>
2022-06-24 08:23:26 -0700

    commit

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -6,8 +6,11 @@
  * @height: height
  */
 
-void free_grid(int **grid, int height);
+void free_grid(int **grid, int height)
 {
 	int i;
 
+	for (i = height - 1; i >= 0; i--)
+		free(grid[i]);
+	free(grid);
 }
