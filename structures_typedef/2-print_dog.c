e8042cf8a6170638814d291b13b2e926c0816834
ismael-soler <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@github.com>
2022-06-28 09:49:18 -0700

    commit

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -14,8 +14,8 @@ void print_dog(struct dog *d)
 		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
-	if (d->age =='\0')
-		printf("Age: (nil)\n");
+	if (d->age == '\0')
+		printf("Age: 0.000000\n");
 	else
 		printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
