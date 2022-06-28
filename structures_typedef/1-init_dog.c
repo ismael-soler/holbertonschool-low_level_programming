a28536fb07f4deb22d35de4492c275439848a523
ismael-soler <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@github.com>
2022-06-28 08:29:39 -0700

    commit

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - calls for dog structure
+ * @d: pointer to my_dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return:  pointer to my_dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
