456cb3a61ed1be217293f5d93d46e9707a7702d1
ismael-soler <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@github.com>
2022-06-29 09:59:02 -0700

    commit

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - write a function that executes a function
+ * @array: array
+ * @size: size
+ * @action: function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array == NULL || size == '\0' || action == NULL)
+		return;
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
+
