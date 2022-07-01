7f79dbda1638ea005dad6040f222cb1711b0cf38
ismael-soler <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@github.com>
2022-07-01 09:44:20 -0700

    commit

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions"
+
+/**
+ * sum_them_all - return the sum of all its parameters
+ * @n: number of extra parameters
+ * Return: total sum
+ */
+
+int sum_them _all(cost unsigned int n, ...)
+{
+	va_list args;
+	int i, sum;
+
+	if (n == 0)
+		return (0);
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+		sum += va_args(args, int);
+
+	va_end(args);
+	return (sum);
+}
