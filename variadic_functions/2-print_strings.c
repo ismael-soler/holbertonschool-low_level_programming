4405fd1abcf5ebbb3c17c41579f8a5a785090247
ismael-soler <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@github.com>
2022-07-01 10:54:45 -0700

    commit

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints string
+ * @separator: string to be printed between strings
+ * @n: number of strings
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	char *temp;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		temp = va_arg(args, char*);
+		if (temp == NULL)
+			printf("(nil)%s", separator);
+		else if (i == n - 1)
+			printf("%s", temp);
+		else
+		{
+			if (separator != NULL)
+				printf("%s%s", temp, separator);
+			else
+				printf("%s", temp);
+		}
+	}
+	printf("\n");
+	va_end(args);
+}
