d33fa353ffabd0240bbb99c5700b597e0b016b2b
ismael-soler <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@github.com>
2022-07-01 10:09:55 -0700

    commit

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		if (i == n - 1)
 			printf("%d\n", va_arg(args, int));
 		else
-			printf("%d%s ", va_arg(args, int), separator);
+			printf("%d%s", va_arg(args, int), separator);
 	}
 	va_end(args);
 }
