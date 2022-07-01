60ea74d194002e0e22a669caaf5766213190d451
ismael-soler <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@github.com>
2022-07-01 09:48:28 -0700

    commit

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -9,14 +9,14 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
-	int i, sum;
+	unsigned int i, sum;
 
 	if (n == 0)
 		return (0);
 
 	va_start(args, n);
 	for (i = 0; i < n; i++)
-		sum += va_args(args, int);
+		sum += va_arg(args, int);
 
 	va_end(args);
 	return (sum);
