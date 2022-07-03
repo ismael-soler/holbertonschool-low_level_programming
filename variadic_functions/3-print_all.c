d588fc8640358ca4869745f8964d909b6dc940da
ismael-soler <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@github.com>
2022-07-03 14:38:00 -0700

    commit

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,38 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints anything
+ * @format: format
+ */
+
+void print_all(const char * const format, ...)
+{
+	int i;
+	char format_char;
+
+	i = 0;
+	while (format[i])
+	{
+		format_char = format[i];
+		get_func(format_char);
+		i++;
+	}
+
+}
+
+/**
+ * get_func - finds tihe correct function to printthe text
+ * @format_char: format character identifier
+ */
+
+void get_func(int format_char)
+{
+	char ar[] = {'c', 'i', 'f', 's'};
+	int i;
+
+	for (i = 0; ar[i]; i++)
+	{
+		if (format_char == ar[i])
+			printf("%c\n", ar[i]);
+	}
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -7,4 +7,7 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+void get_func(int format_char);
 #endif
