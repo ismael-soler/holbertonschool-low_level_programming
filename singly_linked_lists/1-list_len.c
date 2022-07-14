89b96c6f8843da3c50daee825aebcc2bc05ecf6a
ismael-soler <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@github.com>
2022-07-14 08:09:41 -0700

    commit

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * list_len - returns list lenght
+ * @h: pointer
+ * Return: n of nodes
+ */
+
+size_t list_len(const list_t *h)
+{
+	int i;
+	
+	if (h == NULL)
+		return (0);
+	for (i = 0; h->next; i++)
+		h = h->next;
+	return (i + 1);
+}
