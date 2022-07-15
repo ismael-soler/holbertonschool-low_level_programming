a29cf9278d1da196a2ef8e1e9f5f91ac5ac91169
ismael-soler <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@github.com>
2022-07-15 11:50:20 -0700

    commit

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of a list
+ * @head: pointer
+ * Return: value of the deleted node
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *aux;
+	int n;
+
+	if (head)
+	{
+		aux = *head;
+		n = (*head)->n;
+		*head = (*head)->next;
+		free(aux);
+		return (n);
+	}
+	return (0);
+}
