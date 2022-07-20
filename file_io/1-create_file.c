26cb2b53f52988dc690302ed079a4c34b2501c68
ismael-soler <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@github.com>
2022-07-20 11:46:46 -0700

    commit

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	fd = creat(filename, 0600);
-	if (fd == NULL)
+	if (fd == -1)
 		return (-1);
 
 	if (text_content != NULL)
