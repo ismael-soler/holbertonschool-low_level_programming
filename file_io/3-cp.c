e55eaa4d594d777d4ad1fb0e2726dd76457711df
ismael-soler <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@github.com>
2022-07-20 13:46:03 -0700

    commit

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -14,7 +14,7 @@ int main(int ac, char **av)
 	if (ac != 3)
 	{
 		printf("Usage: cp file_from file_to\n");
-		return (97);
+		exit(97);
 	}
 
 	ret = copy(av[1], av[2]);
@@ -30,25 +30,29 @@ int main(int ac, char **av)
 
 int copy(const char *file_from, const char *file_to)
 {
-	int source_fd, dest_fd, close_ret;
+	int source_fd, dest_fd, close_ret, r_out, w_out;
 	char *buffer;
 
-	source_fd = open(file_from, O_RDONLY, 0600);
+	source_fd = open(file_from, O_RDONLY);
 	if (source_fd == -1)
 	{
 		printf("Error: Can't read from file %s\n", file_from);
 		return (98);
 	}
-	dest_fd = open(file_to, O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	dest_fd = open(file_to, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR
+		| S_IRGRP | S_IWGRP | S_IROTH);
 	if (dest_fd == -1)
 	{
 		printf("Error: Can't write to %s\n", file_to);
 		return (99);
 	}
-	buffer = malloc(sizeof(char) * strlen(file_from));
+	buffer = malloc(sizeof(char) * 1024);
 	if (buffer == NULL)
 		return (-1);
 
+	r_out = read(source_fd, buffer, 1024);
+	w_out = write(dest_fd, buffer, r_out);
+	printf("%i\n", w_out);
 	close_ret = close(source_fd);
 	if (close_ret != 0)
 	{
