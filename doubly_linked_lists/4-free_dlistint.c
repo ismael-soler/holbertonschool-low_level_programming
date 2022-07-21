7d3dcb6f2630bf3ea05e0ea127208e6080cee360
ismael-soler <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@github.com>
2022-07-21 12:30:44 -0700

    commit

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a dlistint_t list
+ * @head: pointer to the head
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *aux;
+
+	if (head == NULL)
+	{
+		free(head);
+		return;
+	}
+	for (aux = head; aux; aux = aux->next)
+	{
+		free(aux);
+		aux = aux->next;
+	}
+}
