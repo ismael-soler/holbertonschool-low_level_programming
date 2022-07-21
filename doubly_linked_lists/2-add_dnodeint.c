6e35371027628118633651ccae0cf6c16972ae9a
ismael-soler <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@github.com>
2022-07-21 08:26:47 -0700

    commit

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -10,13 +10,17 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
+	dlistint_t *h_temp;
 
+	h_temp = *head;
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 
+	new_node->prev = NULL;
 	new_node->next = *head;
 	new_node->n = n;
+	h_temp->prev = new_node;
 	*head = new_node;
 	return (new_node);
 }
