0a205affba1577038fae0031f67dab6235303ec0
ismael-soler <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@github.com>
2022-07-21 08:34:31 -0700

    commit

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -22,5 +22,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		(*head)->prev = new_node;
 		new_node->next = *head;
 	}
+	else
+		new_node->next = NULL;
+	*head = new_node;
 	return (new_node);
 }
