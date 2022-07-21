d05b7e0e1a1c28f7566b7e6ec9b4979701cd0075
ismael-soler <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@github.com>
2022-07-21 07:30:15 -0700

    commit

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - adds a node at the beginning of a list
+ * @head: pointer to the head
+ * @n: content of the node
+ * Return: address of the enew element or null if it failed
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->next = *head;
+	new_node->n = n;
+	*head = new_node;
+	return (new_node);
+}
