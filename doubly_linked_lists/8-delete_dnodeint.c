ef48bb6fe6e8025840afe37b9afb241c654ee0f4
ismael-soler <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@github.com>
2022-07-22 16:22:22 -0700

    commit

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -12,7 +12,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int i = 1;
 	dlistint_t *aux0, *aux1, *aux2;
 
-	if (*head == NULL || head == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
 
 	aux1 = *head;
@@ -26,24 +26,22 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (1);
 	}
 
-	for (i = 1; i < index; i++)
-	{
+	for (i = 1; aux1 && i < index; i++)
 		aux1 = aux1->next;
-		if (aux1 == NULL)
-			return (-1);
-	}
-	if (aux1->next == NULL)
+	if (aux1 == NULL || aux1->next == NULL)
+		return (-1);
+	/*if (aux1->next == NULL)
 	{
 		aux0 = aux1->prev;
 		free(aux1);
 		aux0->next = NULL;
 		return (1);
-	}
+	}*/
 	aux2 = aux1->next;
-	aux0 = aux1->prev;
-	aux0->next = aux2;
-	if (aux2)
-		aux2->prev = aux1;
+	aux0 = aux2->next;
+	aux1->next = aux0;
+	if (aux0)
+		aux0->prev = aux1;
 	free(aux1);
 	return (1);
 }
