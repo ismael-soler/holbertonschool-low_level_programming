27b7278fedf322a265a27527cda2fbdea99079f6
ismael-soler <ismaelsolerferreira@gmail.com>
2022-08-21 04:20:53 -0300

    fixed malloc pointer value

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -11,21 +11,23 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table;
 	unsigned long int i = 0;
 
+	if (size <= 0)
+	return (NULL);
+
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 	return (NULL);
 
 	table->size = size;
-	table->array = malloc(size * sizeof(hash_node_t));
+	table->array = malloc(size * sizeof(hash_node_t *));
 	if (table->array == NULL)
 	{
 		free(table);
 		return (NULL);
 	}
 
-	for (i = 0; i < size; i++)
+	for (; i < size; i++)
 	table->array[i] = NULL;
 
 	return (table);
-
 }
