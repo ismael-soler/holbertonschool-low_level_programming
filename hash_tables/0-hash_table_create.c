83abc4984a1c61117d1323c555d17279afbf6d54
ismael-soler <ismaelsolerferreira@gmail.com>
2022-08-21 04:10:52 -0300

    v2

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,14 +9,21 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
-	long unsigned int i = 0;
+	unsigned long int i = 0;
 
-	table = malloc (sizeof(hash_table_t));
+	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
+
 	table->size = size;
-	table->array = malloc(table->size * sizeof(hash_node_t));
-	for (i = 0; i < table->size; i++)
+	table->array = malloc(size * sizeof(hash_node_t));
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
 
 	return (table);
