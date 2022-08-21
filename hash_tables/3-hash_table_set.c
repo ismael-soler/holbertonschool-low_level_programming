654d22ddac240fdbaadac896a9c9b54c62c685c2
ismael-soler <ismaelsolerferreira@gmail.com>
2022-08-21 16:49:57 -0300

    task 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -11,14 +11,36 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = 0;
+	unsigned long int i;
 
 	if (key == NULL)
 	return (0);
 
+	/*get the position of the element on the table*/
 	index = key_index((unsigned char *)key, ht->size);
-	ht->array[index]->key = strdup(key);
-	if (value)
-	ht->array[index]->value = strdup(value);
+
+	/*runs through the array a max of 'size' iterations*/
+	for (i = 0; i < ht->size; ht->array[index] = ht->array[index]->next)
+	{
+		/*checks for the node to exist or is empty*/
+		if (ht->array[index] == NULL) /*if the node doesn't exists*/
+		{
+			ht->array[index] = malloc(sizeof(hash_node_t));
+			if (ht == NULL)
+				return (0);	
+
+			ht->array[index]->key = strdup(key);
+			ht->array[index]->value = strdup(value);
+			ht->array[index]->next = ht->array[index + 1];
+			return (1);
+		}
+		else if (ht->array[index]) /*if the node alredy exists*/
+		{
+			if (strcmp(ht->array[index]->key, key) == 0) /*check if the key matches*/
+				ht->array[index]->value = strdup(value);
+			return (1);
+		}
+	}
 
 	return (1);
 }
