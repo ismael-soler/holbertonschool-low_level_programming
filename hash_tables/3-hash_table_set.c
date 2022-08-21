49b6646b50253d87208bffa9fb526c237091943b
ismael-soler <ismaelsolerferreira@gmail.com>
2022-08-21 17:49:49 -0300

    task 3 v3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -11,41 +11,35 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = 0;
-	unsigned long int i;
+	hash_node_t *current_node = NULL, *new_node = NULL;
 
 	if (key == NULL)
-	return (0);
+		return (0);
 
 	/*get the position of the element on the table*/
 	index = key_index((unsigned char *)key, ht->size);
 
-	/*runs through the array a max of 'size' iterations*/
-	for (i = 0; i < ht->size; ht->array[index] = ht->array[index]->next)
+	current_node = ht->array[index];
+
+	/*runs through the array looking for a key match*/
+	for (; current_node; current_node = current_node->next)
 	{
-		/*checks for the node to exist or is empty*/
-		if (ht->array[index] == NULL) /*if the node doesn't exists*/
+		if (strcmp(current_node->key, key) == 0) /*checks if the key matches*/
 		{
-			ht->array[index] = malloc(sizeof(hash_node_t));
-			if (ht == NULL)
-				return (0);
-
-			ht->array[index]->key = strdup(key);
-			ht->array[index]->value = strdup(value);
-			ht->array[index]->next = NULL;
-			return (1);
-		}
-		else if (ht->array[index]) /*if the node alredy exists*/
-		{
-			if (strcmp(ht->array[index]->key, key) == 0) /*check if the key matches*/
-			{
-				free(ht->array[index]->value);
-				ht->array[index]->value = strdup(value);
-			}
-				ht->array[index] = ht->array[index]->next;
-
+			free(current_node->value);
+			current_node->value = strdup(value);
 			return (1);
 		}
 	}
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+	
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	ht->array[index] = new_node;
+	new_node->next = current_node;
 
 	return (1);
 }
