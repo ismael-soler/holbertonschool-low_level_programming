600c5ddd9f171a3a22487d1500a0646f82c34e4f
ismael-soler <ismaelsolerferreira@gmail.com>
2022-08-21 04:33:42 -0300

    fixed betty

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -4,6 +4,7 @@
  * key_index - returns the index at which the pair should be stored
  * @key: key
  * @size: size of the array of the hash table
+ * Return: index
  */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
