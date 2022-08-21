aec79b2dba14cc4def1e57fbf0b67e11588ea84a
ismael-soler <ismaelsolerferreira@gmail.com>
2022-08-21 04:13:04 -0300

    betty style

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,10 +1,10 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table
- * @size: size of the array
- * Return: pointer to the newly created hash table
- */
+* hash_table_create - creates a hash table
+* @size: size of the array
+* Return: pointer to the newly created hash table
+*/
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
@@ -13,7 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
-		return (NULL);
+	return (NULL);
 
 	table->size = size;
 	table->array = malloc(size * sizeof(hash_node_t));
@@ -24,7 +24,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 
 	for (i = 0; i < size; i++)
-		table->array[i] = NULL;
+	table->array[i] = NULL;
 
 	return (table);
 
