8dc64b81170bae43b47790af6266bb7025b86f4c
ismael-soler <ismaelsolerferreira@gmail.com>
2022-08-30 12:39:17 -0300

    task 0 v2

--- search_algorithms/0-main.c
+++ search_algorithms/0-main.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "search_algos.h"
 
 /**
  * main - Entry point
  *
- * Return: Always EXIT_SUCCESS
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int array[] = {
-        10, 1, 42, 3, 4, 42, 6, 7, -1, 9
-    };
-    size_t size = sizeof(array) / sizeof(array[0]);
+	int array[10] = {
+		8, 1, 42, 3, 4, 42, 6, 7, -1, 9
+	};
 
-    printf("Found %d at index: %d\n\n", 3, linear_search(array, size, 3));
-    printf("Found %d at index: %d\n\n", 42, linear_search(array, size, 42));
-    printf("Found %d at index: %d\n", 999, linear_search(array, size, 999));
-    return (EXIT_SUCCESS);
+	printf("Found at index: %d\n", linear_search(array, 10, 3));
+	return (0);
 }
