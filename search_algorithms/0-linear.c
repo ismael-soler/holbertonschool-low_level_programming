5f483bc4ca172c7b164557a1d86b48e0aa61e4e8
ismael-soler <ismaelsolerferreira@gmail.com>
2022-08-30 12:36:20 -0300

    task 0 v1

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,29 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value in an array of integers
+ * using the Linear search algorithm
+ *
+ * @array: array
+ * @size: size of @array
+ * @value: value to search for
+ * Return: first index where @value is located
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%li] = [%i]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			break;
+		}
+	}
+	return (-1);
+}
